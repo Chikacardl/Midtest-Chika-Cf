7971e197b717fa4eac8cce3b639cf77a0e2d9622
Chikacardl <164526040+Chikacardl@users.noreply.github.com>
2024-04-15 21:02:16 +0700

    Add files via upload

---
+++ PostgreSQL.sql
@@ -0,0 +1,15 @@
+### Tulis query jawaban pada online editor yang telah disarankan atau sesuai dengan preferensi ###
+
+### Notes: anda perlu membuat query table data terlebih dahulu dengan memasukkan query di bawah###
+CREATE TABLE employee_table (
+    employee TEXT NOT NULL,
+    salary INTEGER
+);
+
+INSERT INTO employee_table (employee, salary) VALUES
+('Alice', 11),
+('Benn', 43),
+('Charles', 33),
+('Dorothy', 55),
+('Emma', 22),
+('Franklin', 33);
